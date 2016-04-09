7a8ab38f4ad53c6fbd208744f7e97aafea1544f4
Qisong Wang <songer1993@gmail.com>
2016-04-09 02:05:15 +0100

    initial

---
+++ SentireArduino/SentireArduino.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		SentireArduino.ino
+ Created:	4/9/2016 2:03:30 AM
+ Author:	songer
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
